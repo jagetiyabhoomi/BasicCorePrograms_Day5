411bfde3933bb98cb75af96d327e7d049c3b9372
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-08 18:42:19 +0530

    Checking Odd or even number

---
+++ OddOrEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class OddOrEven
+    {
+        public static void CheckOddEvenNumber()
+        {
+            int num;
+            Console.WriteLine("Enter any number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The Entered Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("The Entered Number is Odd");
+            }
+        }
+    }
+}
