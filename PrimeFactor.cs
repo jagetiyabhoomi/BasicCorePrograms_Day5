833370c87c45ed23d04fdddf36813baac03b9081
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-08 18:47:38 +0530

    Finding Prime Factor of number

---
+++ PrimeFactor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class PrimeFactor
+    {
+        public static void FindPrimeFactor()
+        {
+            int num;
+            Console.WriteLine("Enter any number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i + " is a prime factor of " + num);
+                }
+            }
+        }
+    }
+}
