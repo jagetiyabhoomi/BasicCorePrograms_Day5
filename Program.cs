1445561407b0a4da9bb060c33a4c7d0e605672cc
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-08 18:31:05 +0530

    Calling Harmonic Number Method

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //VowelAndConsonant.CheckTheTypeOfCharacter();
+            //SwapTwoNumbers.SwapNumber();
+            //LargestNumber.CheckLargestNumber();
+            //QuotientRemainder.CheckQuotientRemainder();
+            //OddOrEven.CheckOddEvenNumber();
+            //PrimeFactor.FindPrimeFactor();
+            //LeapYear.FindLeapYear();
+            //FlipCoin.Calculateflips(3);
+            //Powerof2.calculatepower();
+            HarmonicNumber.FindHarmonicNumber();
+        }
+    }
+}
+
